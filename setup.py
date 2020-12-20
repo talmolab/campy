a411956d50b50a47a8830db8a8aa573b359bd424
Diego Aldarondo <diegoaldarondo@g.harvard.edu>
2020-12-20 14:48:12 -0500

    Blacked

--- setup.py
+++ setup.py
@@ -1,19 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-	name='campy',
-	version='0.2.0',
+    name="campy",
+    version="0.2.0",
     packages=find_packages(),
-	install_requires=[
-					'numpy',
-					'imageio',
-					'imageio-ffmpeg',
-					'scikit-image',
-                    'pyyaml',
-					],
-    entry_points={
-        "console_scripts": [
-            "campy-acquire = campy.campy:Main"
-        ]
-    }
+    install_requires=[
+        "numpy",
+        "imageio",
+        "imageio-ffmpeg",
+        "scikit-image",
+        "pyyaml",
+    ],
+    entry_points={"console_scripts": ["campy-acquire = campy.campy:Main"]},
 )
