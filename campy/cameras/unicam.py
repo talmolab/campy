68b93424344de82b09a3b87e50c21d56e615ecca
Talmo Pereira <talmo@princeton.edu>
2022-02-16 20:21:27 -0800

    Change to logging for status reporting

--- campy/cameras/unicam.py
+++ campy/cameras/unicam.py
@@ -8,9 +8,12 @@ import numpy as np
 from collections import deque
 from scipy import io as sio
 from rich.pretty import pprint
-from rich.table import Table
-from rich.live import Live
+from rich.console import Console
 from datetime import timedelta
+import traceback
+
+
+console = Console()
 
 
 def ImportCam(make):
@@ -38,10 +41,9 @@ def LoadSystems(params):
             cam = ImportCam(makes[m])
             systems[makes[m]]["system"] = cam.LoadSystem(params)
     except Exception as e:
-        logging.error(
-            "Caught exception at camera/unicam.py LoadSystems. Check cameraMake: {}".format(
-                e
-            )
+        console.log(
+            "Caught exception at camera/unicam.py LoadSystems. "
+            "Check cameraMake:\n" + traceback.format_exc()
         )
         raise
     return systems
@@ -52,10 +54,9 @@ def LoadDevice(systems, params, cam_params):
         cam = ImportCam(cam_params["cameraMake"])
         cam_params = cam.LoadDevice(systems, params, cam_params)
     except Exception as e:
-        logging.error(
-            "Caught exception at camera/unicam.py LoadSystems. Check cameraMake: {}".format(
-                e
-            )
+        console.log(
+            "Caught exception at camera/unicam.py LoadDevice. "
+            "Check cameraMake:\n" + traceback.format_exc()
         )
         raise
     return cam_params
@@ -64,7 +65,7 @@ def LoadDevice(systems, params, cam_params):
 def OpenCamera(cam_params, stopWriteQueue):
     # Import the cam module
     cam = ImportCam(cam_params["cameraMake"])
-    
+
     try:
         camera, cam_params = cam.OpenCamera(cam_params)
 
@@ -78,7 +79,9 @@ def OpenCamera(cam_params, stopWriteQueue):
         )
 
     except Exception as e:
-        logging.exception("Caught error at cameras/unicam.py OpenCamera:")
+        console.log(
+            "Caught error at cameras/unicam.py OpenCamera:\n" + traceback.format_exc()
+        )
         stopWriteQueue.append("STOP")
 
     return cam, camera, cam_params
@@ -144,50 +147,50 @@ def StartGrabbing(camera, cam_params, cam):
     return grabbing
 
 
-def CountFPS(grabdata, frameNumber, timeStamp, status_table, live_display):
+def CountFPS(grabdata, frameNumber, timeStamp):
     if frameNumber % grabdata["chunkLengthInFrames"] == 0:
-        session_elapsed = timeStamp - grabdata["timeStamp"][0]
-        session_elapsed_min, session_elapsed_sec = divmod(session_elapsed, 60)
+        elapsed = timeStamp - grabdata["timeStamp"][0]
+        elapsed_min, elapsed_sec = divmod(elapsed, 60)
+        elapsed_hour, elapsed_min = divmod(elapsed_min, 60)
+
         frame_count = len(grabdata["timeStamp"])
-        dropped = frame_count - frameNumber
+
         ts = grabdata["timeStamp"]
         if len(ts) > grabdata["chunkLengthInFrames"]:
-            ts = ts[-grabdata["chunkLengthInFrames"]:]
+            ts = ts[-grabdata["chunkLengthInFrames"] :]
         dts = np.abs(np.diff(ts))
         fps = 1 / dts
         fps_avg = fps.mean()
         fps_std = fps.std()
 
-        status_table.add_row(
-            f"{int(session_elapsed_min):02}:{int(round(session_elapsed_sec)):02}",
-            f"{frameNumber:,}",
-            f"{frame_count:,}",
-            f"{dropped:,}",
-            f"{fps_avg:.1f} +- {fps_std:.1f}",
+        expected_frames = (ts[-1] - ts[0]) // grabdata["frameRate"]
+        expected_delta = expected_frames - grabdata["chunkLengthInFrames"]
+        expected_col = "bright_green" if abs(expected_delta) < 3 else "red"
+
+        sep = " [grey70]|[/grey70] "
+        console.log(
+            "".join(
+                [
+                    "[bold]Camera:[/bold] ",
+                    grabdata["cameraName"],
+                    sep,
+                    f"[bold]Session:[/bold] {elapsed_hour:02}:{elapsed_min:02}:{elapsed_sec:02}",
+                    sep,
+                    f"[bold]Frames:[/bold] {frame_count:,}",
+                    sep,
+                    f"[bold]FPS:[/bold] {fps_avg:.1f} +- {fps_std:.1f}",
+                    sep,
+                    f"[bold]Expected:[/bold] [{expected_col}]{expected_delta}[/{expected_col}]",
+                ]
+            )
         )
-
-        if live_display is None:
-            live_display = Live(status_table)
-            live_display.start()
-
-    return live_display
-
-
-def setup_status_table():
-    table = Table(show_footer=False)
-    table.add_column("Session")
-    table.add_column("Frame")
-    table.add_column("Grabs")
-    table.add_column("Drops")
-    table.add_column("FPS")
-    return table
 
 
 def GrabFrames(cam_params, writeQueue, dispQueue, stopReadQueue, stopWriteQueue):
     # Open the camera object
     cam, camera, cam_params = OpenCamera(cam_params, stopWriteQueue)
 
-    print("Camera parameters:")
+    console.log("Camera parameters:")
     pprint(cam_params)
     print()
 
@@ -201,19 +204,12 @@ def GrabFrames(cam_params, writeQueue, dispQueue, stopReadQueue, stopWriteQueue)
     # Start grabbing frames from the camera
     grabbing = StartGrabbing(camera, cam_params, cam)
 
-    # Setup status logging
-    status_table = setup_status_table()
-    live_display = None
-
     frameNumber = 0
     while not stopReadQueue:
         try:
             # Grab image from camera buffer if available
             grabResult = cam.GrabFrame(camera, frameNumber)
-
-            # Append numpy array to writeQueue for writer to append to file
             img = cam.GetImageArray(grabResult)
-            writeQueue.append(img)
 
             # Append timeStamp and frameNumber to grabdata
             frameNumber += 1
@@ -221,11 +217,14 @@ def GrabFrames(cam_params, writeQueue, dispQueue, stopReadQueue, stopWriteQueue)
             timeStamp = cam.GetTimeStamp(grabResult)
             grabdata["timeStamp"].append(timeStamp)
 
+            # Append numpy array to writeQueue for writer to append to file
+            writeQueue.append((frameNumber, timeStamp, img))
+
             # Display converted, downsampled image in the Window
             if frameNumber % grabdata["frameRatio"] == 0:
                 img = cam.DisplayImage(cam_params, dispQueue, grabResult)
 
-            live_display = CountFPS(grabdata, frameNumber, timeStamp, status_table, live_display)
+            CountFPS(grabdata, frameNumber, timeStamp)
 
             cam.ReleaseFrame(grabResult)
 
@@ -234,13 +233,11 @@ def GrabFrames(cam_params, writeQueue, dispQueue, stopReadQueue, stopWriteQueue)
 
         except Exception as e:
             if cam_params["cameraDebug"]:
-                logging.error(
-                    "Caught exception at cameras/unicam.py GrabFrames: {}".format(e)
+                console.log(
+                    "Caught exception at cameras/unicam.py GrabFrames:\n"
+                    + traceback.format_exc()
                 )
             time.sleep(0.001)
-
-    if live_display is not None:
-        live_display.stop()
 
     # Close the camaera, save metadata, and tell writer and display to close
     cam.CloseCamera(cam_params, camera)
@@ -262,10 +259,9 @@ def SaveMetadata(cam_params, grabdata):
         frame_count = grabdata["frameNumber"][-1]
         time_count = grabdata["timeStamp"][-1]
         fps_count = frame_count / time_count
-        print(
-            "{} saved {} frames at {} fps.".format(
-                cam_params["cameraName"], frame_count, fps_count
-            )
+        console.log(
+            f"Camera {cam_params['cameraName']} saved {frame_count} "
+            f"frames at {fps_count} fps."
         )
 
         meta = cam_params
@@ -294,16 +290,16 @@ def SaveMetadata(cam_params, grabdata):
                 if isinstance(row[1], (list, str, int, float)):
                     w.writerow(row)
 
-        print("Saved metadata for {}.".format(cam_params["cameraName"]))
+        console.log("Saved metadata for", cam_params["cameraName"])
 
     except Exception as e:
-        logging.error("Caught exception: {}".format(e))
+        console.log("Caught exception:\n" + traceback.format_exc())
 
 
 def CloseSystems(systems, params):
-    print("Closing systems...")
+    console.log("Closing systems...")
     makes = GetMakeList(params)
     for m in range(len(makes)):
         cam = ImportCam(makes[m])
         cam.CloseSystem(systems[makes[m]]["system"], systems[makes[m]]["deviceList"])
-    print("Exiting campy...")
+    console.log("Exiting campy...")
