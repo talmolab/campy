86be8cb55314c5b3fb08a562a91d17acf5c5e30d
Talmo Pereira <talmo@salk.edu>
2022-03-10 09:13:11 -0800

    Fix scheduled recording script

--- scheduled_recording.bat
+++ scheduled_recording.bat
@@ -1,3 +1,5 @@
-SET log_file=%cd%\recording_logs.txt
-call C:\Miniconda3\envs\campy\Scripts\activate.bat
+CD C:\code\campy
+SET log_date=%DATE:~10,4%%DATE:~4,2%%DATE:~7,2%
+SET log_file=%cd%\recording_logs.%log_date%.txt
+CALL  C:\Miniconda3\Scripts\activate.bat "C:\Miniconda3\envs\campy"
 campy-acquire configs\campy_config_4cam_minicam.4xGPU.yaml > %log_file%
