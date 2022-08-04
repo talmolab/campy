8de43c999ad844d8df85274006f1a74f8f6ecb7a
Talmo Pereira <talmo@salk.edu>
2022-08-03 19:51:56 -0700

    Add more configs

--- scheduled_recording.bat
+++ scheduled_recording.bat
@@ -2,4 +2,5 @@ CD C:\code\campy
 SET log_date=%DATE:~10,4%%DATE:~4,2%%DATE:~7,2%
 SET log_file=%cd%\recording_logs.%log_date%.txt
 CALL  C:\Miniconda3\Scripts\activate.bat "C:\Miniconda3\envs\campy"
-campy-acquire configs\campy_config_4cam_minicam.4xGPU.yaml > %log_file%
+rem campy-acquire configs\campy_config_4cam_minicam.4xGPU.yaml > %log_file%
+campy-acquire configs\campy_config_3cam_minicam.GPU.yaml > %log_file%
